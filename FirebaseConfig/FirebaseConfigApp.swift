b6f0d024fe3201c5df5e6f8ef47fd98ccfc3be5b
Salih Cakmak <slhckmk0@gmail.com>
2023-09-25 15:13:07 +0300

    Initial Commit

---
+++ FirebaseConfig/FirebaseConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseConfigApp.swift
+//  FirebaseConfig
+//
+//  Created by Salih on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
